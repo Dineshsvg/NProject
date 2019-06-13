9e38282b0634761286c700eb2bf1ea3ad63827a2
vinoth <vinothvgp@gmail.com>
2019-06-13 12:36:22 +0530

    comment filter

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,7 +5,7 @@ validates_presence_of :name,:dob
 validates_format_of :email_id, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/, :if => Proc.new {|u| !u.email_id.blank?}
 validates_uniqueness_of :phone_no
 
- def before_save_member
-raise params.inspect
- end
+# def before_save_member
+#raise params.inspect
+# end
 end
